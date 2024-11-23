ce64368647e0599b28588e9ae27967bc19097b1c
DouglasB <douglas_borges09@hmail.com>
2024-11-23 08:13:28 -0300

    chore: add setup and  ui dhadcn and tailwind pluging

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+// import "./global.css";
+
+function App() {
+  return (
+    <div className="justify-centerflex h-screen w-screen flex-col items-center gap-4">
+      <p>Bem vindo </p>
+      <Button>DOuglas amanha vc coda tudo !</Button>
+    </div>
+  );
+}
+
+export default App;
